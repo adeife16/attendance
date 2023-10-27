876834c6dd94b4b520f91d65da4337c386a95417
Opeyemi Dada <dadaadeife@gmail.com>
2023-10-27 11:47:06 +0100

    Setup

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	$title = "Dashboard";
 	require_once 'header.php';
 ?>
@@ -6,12 +6,12 @@
         <div class="col-md-12">
           <div class="card shadow mb-4">
               <div class="card-body text-center">
-
+                <h1>WEB BASED ATTENDANCE SYSTEM</h1>
               </div>
             </div>
           </div>
         </div>
       </div>
-<?php 
+<?php
 	require_once 'footer.php';
 ?>
